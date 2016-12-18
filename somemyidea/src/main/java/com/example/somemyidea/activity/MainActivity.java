2684989e45399b2595f66526473e5f29d6492c7d
ShayPatrickCormac <574583006@qq.com>
2016-12-19 00:05:40 +0800

    修改V7控件的颜色

--- somemyidea/src/main/java/com/example/somemyidea/activity/MainActivity.java
+++ somemyidea/src/main/java/com/example/somemyidea/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.somemyidea.activity;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,6 +8,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.AccelerateInterpolator;
 import android.widget.Button;
 import android.widget.CheckedTextView;
 import android.widget.ToggleButton;
@@ -40,6 +43,26 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) 
+        {
+            btnTotast.setOutlineProvider(new ViewOutlineProvider() 
+            {
+                @Override
+                public void getOutline(View view, Outline outline) 
+                {
+                    int shapeSize = (int) getResources().getDimension(R.dimen.fab_margin);
+                    shapeSize = btnTotast.getWidth();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) 
+                    {
+                      //  outline.setRoundRect(0, 0, shapeSize / 2, shapeSize / 2, shapeSize);
+                        outline.setRect(0, 0, shapeSize, shapeSize/2);
+                        
+                    }
+                }
+            });
+            btnTotast.setClipToOutline(true);
+        }*/
+        
 
         bindListener();
     }
@@ -48,8 +71,13 @@ public class MainActivity extends AppCompatActivity {
     {
         btnTotast.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,ToastActivity.class));
+            public void onClick(View v) 
+            {
+                //Circular Reveal
+                Animator animator = createAnimator(v);
+                if (animator!=null)
+                    animator.start();
+              //  startActivity(new Intent(MainActivity.this,ToastActivity.class));
             }
         });
         
@@ -71,6 +99,20 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,ViewStubTestActivity.class));
             }
         });
+    }
+    
+    
+    public Animator createAnimator(View v)
+    {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) 
+        {
+            Animator animator = ViewAnimationUtils.createCircularReveal(v, v.getWidth() / 2, v.getHeight() / 2, v.getWidth(), 0);
+            animator.setInterpolator(new AccelerateInterpolator());
+            animator.setDuration(500);
+            return animator;
+        }
+        return null;
+
     }
 
 }
