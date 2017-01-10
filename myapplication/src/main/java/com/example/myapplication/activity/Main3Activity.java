54c61f313e3b025c3e2c1bb167e880d6147e78a2
ShayPatrickCormac <574583006@qq.com>
2017-01-10 21:40:27 +0800

    动画

--- myapplication/src/main/java/com/example/myapplication/activity/Main3Activity.java
+++ myapplication/src/main/java/com/example/myapplication/activity/Main3Activity.java
@@ -1,23 +1,57 @@
 package com.example.myapplication.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
 
-public class Main3Activity extends AppCompatActivity {
+public class Main3Activity extends AppCompatActivity
+{
+
+    private LinearLayout llContainer;
+    private Button btnAdd;
+    private Button btnRemove;
+    private AppCompatActivity activity;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
         LayoutInflater.from(this).inflate(R.layout.activity_main3, null);
        // LayoutInflaterCompat.setFactory();
        //LayoutInflaterFactory
-        Intent intent = new Intent(this, Main3Activity.class);
-        startActivity(intent);
-       // startActivityForResult();
+        activity = this;
+        btnAdd = (Button) findViewById(R.id.btnAddPropertityAnimation);
+        
+        
+       btnRemove =   (Button) findViewById(R.id.btnRemovePropertityAnimation);
+
+        llContainer = (LinearLayout) findViewById(R.id.llContainer);
+        bindListener();
+    }
+
+    private void bindListener() {
+        
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = new TextView(activity);
+                textView.setText("哈哈");
+                llContainer.addView(textView);
+            }
+        });
+        btnRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+               llContainer.removeAllViews();
+            }
+        });
     }
 }
