c2f6800a98501b6124e416965d9ec7329e432e4f
shaycormac <574583006@qq.com>
2017-01-11 18:03:40 +0800

    Merge branch 'master' of https://git.oschina.net/shaycormac/TestBehavior
    # Conflicts:
    #	myapplication/src/main/java/com/example/myapplication/activity/Main2Activity.java

--- myapplication/src/main/java/com/example/myapplication/activity/Main3Activity.java
+++ myapplication/src/main/java/com/example/myapplication/activity/Main3Activity.java
@@ -2,8 +2,11 @@ package com.example.myapplication.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.myapplication.R;
+import com.example.myapplication.utils.StatusBarUtil;
 
 /**
  * 测试hierarchy view 树型图
@@ -14,5 +17,8 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        View llLayout=findViewById(R.id.activity_main3);
+       ImageView imageView = (ImageView) findViewById(R.id.imgStatusBar);
+        StatusBarUtil.setTranslucentForImageView(this,llLayout);
     }
 }
