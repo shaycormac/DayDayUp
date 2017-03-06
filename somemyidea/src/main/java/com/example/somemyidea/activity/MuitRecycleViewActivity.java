7a91cfb21433ce7a12727ed794f7b1f0fb64374c
shaycormac <574583006@qq.com>
2017-03-06 09:05:54 +0800

    戴菲菲

---
+++ somemyidea/src/main/java/com/example/somemyidea/activity/MuitRecycleViewActivity.java
@@ -0,0 +1,15 @@
+package com.example.somemyidea.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.somemyidea.R;
+
+public class MuitRecycleViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_muit_recycle_view);
+    }
+}
